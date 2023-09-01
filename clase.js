4c9a60b1f9391db91e52e6cc7b98d4006f1f5ea6
miacardenas08 <136343840+miacardenas08@users.noreply.github.com>
2023-09-01 08:52:53 -0300

    Add files via upload

---
+++ clase.js
@@ -0,0 +1,56 @@
+//1)
+function cuaadrado(num)
+{
+    return num * num
+}
+
+//2)
+ function cubo(num)
+ {
+     return num * num * num
+ }
+
+
+// 3) a) x^2-x-10
+function operacionA(x) 
+{
+   // let reultado = cuadrado(x) - x - 10
+   let resultado = (x * x) - x - 10
+   return (x*x) - x - 10
+}
+
+//3)  b) (x-3)*2
+function operacionB(x)
+{
+    let resultado = (x-3)*2
+    return resultado
+}
+
+//3) c) x*x^2+20
+function operacionC(x)
+{
+    let resultado = (x * x * x) + 20
+    return resultado
+}
+
+//3) d) (x/2)+5
+function operacionD(x)
+{
+    let resultado = (x / x) + 5
+    return resultado
+}
+
+//3) e) x^3+x^2+10
+function operacionE(x)
+{
+    let resultado = (x * x * x) + (x * x) + 10
+    return resultado
+}
+
+//3) f) (x*10)*5
+function operacionF(x)
+{
+    let resultado = (x*10)*5
+    return resultado
+}
+
